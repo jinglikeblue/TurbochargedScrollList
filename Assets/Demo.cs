290d51f978d279e8b4725978bfe40be944d88731
Jing <122300890@qq.com>
2019-11-30 00:19:19 +0800

    update

--- Assets/Demo.cs
+++ Assets/Demo.cs
@@ -17,7 +17,7 @@ public class Demo : MonoBehaviour
         Application.targetFrameRate = 60;
 #endif
 
-        var datas = new int[500];
+        var datas = new int[1600];
         for(int i = 0; i < datas.Length; i++)
         {
             datas[i] = id++;
--- Assets/TurbochargedScrollList/GridScrollList.cs
+++ Assets/TurbochargedScrollList/GridScrollList.cs
@@ -5,6 +5,47 @@ using UnityEngine.UI;
 
 namespace Jing.TurbochargedScrollList
 {
+    struct GridPos
+    {
+
+        public int x { get; private set; }
+
+        public int y { get; private set; }
+
+        public float pixelX { get; private set; }
+
+        public float pixelY { get; private set; }
+
+        public int index { get; private set; }
+
+        int _gridColCount;
+        float _gridW;
+        float _gridH;
+
+        public GridPos(int x, int y, int gridColCount, int gridRowCount, float gridW, float gridH)
+        {
+            this._gridColCount = gridColCount;
+            this._gridW = gridW;
+            this._gridH = gridH;
+
+            this.x = x >= gridColCount ? gridColCount - 1 : x;
+            this.y = y >= gridRowCount ? gridRowCount - 1: y;
+            this.pixelX = x * _gridW;
+            this.pixelY = y * _gridH;
+            this.index = y * _gridColCount + x;
+        }
+
+        public void ChangePos(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.pixelX = x * _gridW;
+            this.pixelY = y * _gridH;
+            this.index = y * _gridColCount + x;
+        }
+    }
+
+
     public enum EGridConstraint
     {
         /// <summary>
@@ -60,6 +101,17 @@ namespace Jing.TurbochargedScrollList
         /// </summary>
         public int constraintCount { get; private set; }
 
+
+        /// <summary>
+        /// 行数
+        /// </summary>
+        public int rowCount { get; private set; }
+
+        /// <summary>
+        /// 列数
+        /// </summary>
+        public int colCount { get; private set; }
+
         public GridScrollList(GameObject scrollView, OnRenderItem itemRender, Vector2 gap, EGridConstraint constraint, int constraintCount = 0)
         {
             this.gap = gap;
@@ -81,23 +133,30 @@ namespace Jing.TurbochargedScrollList
             this.constraintCount = constraintCount;
 
             Init(scrollView, itemPrefab, itemRender);
-
-            var glg = content.gameObject.AddComponent<GridLayoutGroup>();
-            glg.spacing = gap;
-            glg.cellSize = itemPrefab.GetComponent<RectTransform>().sizeDelta;
-            var csf = content.gameObject.AddComponent<ContentSizeFitter>();
-            csf.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
         }
 
         protected override bool AdjustmentItemSize(ScrollListItem item)
         {
+            if (item.width != itemDefaultfSize.x || item.height != itemDefaultfSize.y)
+            {
+                item.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, itemDefaultfSize.x);
+                item.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, itemDefaultfSize.y);
+            }
             return false;
         }
 
         protected override void Refresh(UpdateData updateConfig, out int lastStartIndex)
         {
             var scrollX = -content.localPosition.x;
-            var scrollY = content.localPosition.y;                       
+            if (scrollX < 0)
+            {
+                scrollX = 0;
+            }
+            var scrollY = content.localPosition.y;
+            if (scrollY < 0)
+            {
+                scrollY = 0;
+            }
 
             switch (constraint)
             {
@@ -110,7 +169,7 @@ namespace Jing.TurbochargedScrollList
                     break;
                 default:
                     throw new Exception($"Wrong constraint:{constraint}");
-            }            
+            }
         }
 
         /// <summary>
@@ -128,11 +187,51 @@ namespace Jing.TurbochargedScrollList
             int startIndex = 0;
 
             //根据滚动区域左上角算出起始item的二维位置;
-            Vector2Int startPos = new Vector2Int((int)(scrollX / bigW), (int)(scrollY / bigH));
+            GridPos startPos = new GridPos((int)(scrollX / bigW), (int)(scrollY / bigH), colCount, rowCount, bigW, bigH);
+            startIndex = startPos.index;
             //根据滚动区域左下角算出结束item的二维位置
-            Vector2Int endPos = new Vector2Int((int)(scrollX + content.rect.width / bigW), (int)(scrollY + content.rect.height / bigH));
+            GridPos endPos = new GridPos((int)((scrollX + viewportSize.x) / bigW), (int)((scrollY + viewportSize.y) / bigH), colCount, rowCount, bigW, bigH);
             //计算出关联的所有Item的索引
+            List<GridPos> list = new List<GridPos>();
+            for (int y = startPos.y; y <= endPos.y; y++)
+            {
+                for (int x = startPos.x; x <= endPos.x; x++)
+                {
+                    var gp = new GridPos(x, y, colCount,rowCount, bigW, bigH);
+                    if (gp.index < _itemModels.Count)
+                    {
+                        list.Add(gp);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
 
+            /// <summary>
+            /// 最后一次显示的Item的缓存
+            /// </summary>
+            Dictionary<ScrollListItemModel<TData>, ScrollListItem> lastShowingItems = new Dictionary<ScrollListItemModel<TData>, ScrollListItem>(_showingItems);
+
+            _showingItems.Clear();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var gridPos = list[i];
+                var model = _itemModels[gridPos.index];
+
+                ScrollListItem item = CreateItem(model, gridPos.index, lastShowingItems);
+                _showingItems[model] = item;
+                item.rectTransform.localPosition = new Vector3(gridPos.pixelX, -gridPos.pixelY, 0);
+            }
+
+            //回收没有使用的item
+            foreach (var item in lastShowingItems.Values)
+            {
+                item.gameObject.SetActive(false);
+                _recycledItems.Add(item);
+            }
 
             return startIndex;
         }
@@ -156,11 +255,14 @@ namespace Jing.TurbochargedScrollList
         protected override void ResizeContent(UpdateData updateConfig)
         {
             float contentW = 0;
-            float contentH = 0 ;
+            float contentH = 0;
             int itemAmount = _itemModels.Count;
 
             float bigW = itemDefaultfSize.x + gap.x;
-            float bigH = itemDefaultfSize.y + gap.y;            
+            float bigH = itemDefaultfSize.y + gap.y;
+
+            colCount = 0;
+            rowCount = 0;
 
             //计算Content大小
             switch (constraint)
@@ -170,99 +272,39 @@ namespace Jing.TurbochargedScrollList
                     //计算出constraintCount
                     constraintCount = (int)((contentW + gap.x) / bigW);
                     //确定高度,通过item总数和constraintCount算出                    
-                    if(itemAmount > 0)
+                    if (itemAmount > 0)
                     {
-                        int row = (itemAmount - 1) / constraintCount + 1;
-                        contentH = row * bigH - gap.y;
-                    }                    
+                        rowCount = (itemAmount - 1) / constraintCount + 1;
+                        contentH = rowCount * bigH - gap.y;
+                    }
+
+                    colCount = constraintCount;
                     break;
                 case EGridConstraint.FIXED_COLUMN_COUNT: //根据列数确定Content大小
                     contentW = constraintCount * (itemDefaultfSize.x + gap.x) - gap.x;
                     //确定高度,通过item总数和constraintCount算出
                     if (itemAmount > 0)
                     {
-                        int row = (itemAmount - 1) / constraintCount + 1;
-                        contentH = row * bigH - gap.y;
+                        rowCount = (itemAmount - 1) / constraintCount + 1;
+                        contentH = rowCount * bigH - gap.y;
                     }
+
+                    colCount = constraintCount;
                     break;
                 case EGridConstraint.FIXED_ROW_COUNT: //根据行数确定Content大小
                     contentH = constraintCount * (itemDefaultfSize.y + gap.y) - gap.y;
                     //确定宽度,通过item总数和constraintCount算出                    
-                    if(itemAmount > 0)
+                    if (itemAmount > 0)
                     {
-                        int col = (itemAmount - 1) / constraintCount + 1;
-                        contentW = col * bigW - gap.x;
+                        colCount = (itemAmount - 1) / constraintCount + 1;
+                        contentW = colCount * bigW - gap.x;
                     }
+
+                    rowCount = constraintCount;
                     break;
             }
 
             SetContentSize(contentW, contentH);
-        }
-
-        ScrollListItem Create(TData data)
-        {
-            var go = GameObject.Instantiate(itemPrefab, content);
-            var item = go.AddComponent<ScrollListItem>();
-            item.rectTransform.anchorMin = Vector2.up;
-            item.rectTransform.anchorMax = Vector2.up;
-            item.rectTransform.pivot = Vector2.up;
-            item.gameObject.name = $"item_{content.transform.childCount}";
-            item.data = data;
-            item.index = item.transform.GetSiblingIndex();
-            return item;
-        }
-
-        public override void AddRange(IEnumerable<TData> collection)
-        {
-            foreach (var data in collection)
-            {
-                Add(data);
-            }
-        }
-
-        public override void Add(TData data)
-        {
-            RenderItem(Create(data), data);
-        }
-
-        public override void Insert(int index, TData data)
-        {
-            var item = Create(data);
-            item.transform.SetSiblingIndex(index);
-            item.index = index;
-            RenderItem(item, data);
-            MarkDirty();
-        }
-
-        public override bool Remove(TData data)
-        {
-            for (int i = 0; i < content.transform.childCount; i++)
-            {
-                var t = content.transform.GetChild(i);
-                if (t.GetComponent<ScrollListItem>().data.Equals(data))
-                {
-                    GameObject.Destroy(t.gameObject);
-                    MarkDirty();
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public override void RemoveAt(int index)
-        {
-            var t = content.transform.GetChild(index);
-            GameObject.Destroy(t.gameObject);
-            MarkDirty();
-        }
-
-        public override void Clear()
-        {
-            for (int i = 0; i < content.transform.childCount; i++)
-            {
-                var t = content.transform.GetChild(i);
-                GameObject.Destroy(t.gameObject);
-            }
         }
     }
 }
